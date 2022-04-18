4c9136c392fd6ad83e14d2cbfab463bd74181c3d
Tosin17 <kajopelayeola@gmail.com>
2022-04-18 15:15:38 -0400

    Init lambda

---
+++ src/handler.ts
@@ -0,0 +1,43 @@
+import * as AWS from "aws-sdk";
+import { APIGatewayProxyEvent, SNSEvent } from "aws-lambda";
+import { logger } from "./logger";
+
+const throwRandomError = () => {
+  const n = Math.floor(Math.random() * 3 + 1);
+  logger.info(`Random error - ${n}`);
+  if (n === 2) {
+    throw new Error("Something bad happend");
+  }
+};
+
+export const saveGame = async (event: APIGatewayProxyEvent): Promise<any> => {
+  try {
+    logger.info({ message: "Publishing" });
+    throwRandomError();
+
+    await new AWS.SNS()
+      .publish({
+        Message: "Some game saved",
+        TopicArn: "arn:aws:sns:us-east-1:907686965662:SavedGameTopic",
+      })
+      .promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: "Game saved",
+        },
+        null,
+        2
+      ),
+    };
+  } catch (error) {
+    logger.error(JSON.stringify(error));
+    throw Error(error);
+  }
+};
+
+export const sendEmail = async (event: SNSEvent) => {
+  logger.info("Email Sent", JSON.stringify(event.Records));
+};
